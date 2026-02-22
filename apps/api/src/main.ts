d3051727656a1f10e52a255405662c3a534373f5
Hadi Nurhidayat <hadi.nurhidayat@paramatech.id>
2026-02-22 16:18:48 +0700

    INITIAL COMMIT

---
+++ apps/api/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3001;
+
+  await app.listen(port);
+  console.log('API is running on port ' + port);
+}
+bootstrap();
